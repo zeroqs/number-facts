902683d206a9c40d9f880e333254dbcc8cec7bee
zeroqs <smthq@bk.ru>
2022-09-23 20:11:12 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { store } from './redux/store'
-import { Provider } from 'reacgit t-redux'
+import { Provider } from 'react-redux'
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
